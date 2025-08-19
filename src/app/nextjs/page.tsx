ae980820b23f3ee942ad7db04982f4c57f1d34b2
gerardoposadagp <gerardo.posada.gp@gmail.com>
2025-08-18 19:20:24 -0500

    refactor: simplify Dashboard component structure by removing unnecessary divs

--- src/app/nextjs/page.tsx
+++ src/app/nextjs/page.tsx
@@ -1,9 +1,9 @@
 const Dashboard = () => {
   return (
-      <>
-          <div className="text-xl">Tema1 Content</div>
-          <div className="text-base">Descripcion Tema1 Content</div>
-      </>
+    <>
+      <div className="text-xl">Tema1 Content</div>
+      <div className="text-base">Descripcion Tema1 Content</div>
+    </>
   );
 };
 
