66073efde3775f0f8e5a989bdd1a950938068ec6
gerardoposadagp <gerardo.posada.gp@gmail.com>
2025-08-23 20:09:49 -0500

    refactor: update SidebarRender component to use DocumentDuplicateIcon directly and clean up code structure

--- src/app/components/navbars/SidebarRender.tsx
+++ src/app/components/navbars/SidebarRender.tsx
@@ -27,32 +27,35 @@ export default function SidebarRender({
   linkProps: LinkNavbarType;
 }) {
   const pathname = usePathname();
-  const LinkIcon = iconMap["DocumentDuplicateIcon"];
-
-  // console.log(">>> Sidebar renderizado");
 
   return (
     <nav>
       <ul>
-        {linkProps.map((link, i) => (
-          <li key={i}>
-            <Link
-              key={i}
-              href={{
-                pathname: link.href
-              }}
-              className={clsx(
-                "flex h-[48px] grow mt-2 items-center text-white-600 justify-center gap-2 rounded-md bg-gray-150 p-3 text-sm font-medium hover:bg-sky-300 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-                {
-                  "bg-sky-100 text-blue-900": pathname === link.href,
-                }
-              )}
-            >
-              <LinkIcon className="w-5" />
-              <p className="hidden md:block">{link.label}</p>
-            </Link>
-          </li>
-        ))}
+        {linkProps.map((link, i) => {
+          // const Icon = DocumentTextIcon;
+          // const Icon = link.icon
+          //   ? iconMap[link.icon as keyof typeof iconMap]
+          //   : DocumentDuplicateIcon;
+
+          return (
+            <li key={i}>
+              <Link
+                href={{
+                  pathname: link.href,
+                }}
+                className={clsx(
+                  "flex h-[48px] grow mt-2 items-center text-white-600 justify-center gap-2 rounded-md bg-gray-150 p-3 text-sm font-medium hover:bg-sky-300 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                  {
+                    "bg-sky-100 text-blue-900": pathname === link.href,
+                  }
+                )}
+              >
+                <DocumentDuplicateIcon className="w-6 h-6 flex-shrink-0 min-w-[24px] min-h-[24px]" />
+                <p className="hidden md:block">{link.label}</p>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
--- src/app/types/NavBarsTypes.ts
+++ src/app/types/NavBarsTypes.ts
@@ -1,6 +1,7 @@
 type LinkNavbarType = {
   label: string;
   href: string;
+  // icon?: keyof typeof import("@heroicons/react/24/outline");
 }[];
 
 export type { LinkNavbarType };
